1baa8f2cdec03c018ee59dafbecba53d9825c3d2
Ivan Grigoriy Yakubovich <152319727+iyakubovich03@users.noreply.github.com>
2026-09-22 20:27:48 -0400

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self):
+        self.adj={}
+        self.word=False
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.prefix=Node()
+        
+
+    def insert(self, word: str) -> None:
+        reference=self.prefix
+        for w in word:
+            if w not in reference.adj:
+                reference.adj[w]=Node()
+            reference=reference.adj[w]
+        reference.word=True
+
+
+    def search(self, word: str) -> bool:
+        ref=self.check(word)
+        return ref.word if ref else False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        ref=self.check(prefix)
+        return ref is not None
+
+    def check(self,prefix):
+        reference=self.prefix
+        for w in prefix:
+            if w not in reference.adj:
+                return None
+            reference=reference.adj[w]
+        return reference
+
+
+        
+        
